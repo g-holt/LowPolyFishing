ea74ccce52489e7187cdb2e6c45c856fd808bdef
Grayson Holt <gholtwork@gmail.com>
2022-03-25 17:07:20 -0500

    Added New Input System

--- LowPolyFishing/Assets/Scripts/PlayerMovement.cs
+++ LowPolyFishing/Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float moveSpeed = 5f;
+
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
