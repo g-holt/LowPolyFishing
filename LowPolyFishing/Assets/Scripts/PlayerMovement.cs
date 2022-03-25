338be4dd5e28180b3ec5956bdd50860d4de4a8de
Grayson Holt <gholtwork@gmail.com>
2022-03-25 17:30:36 -0500

    Update PlayerMovement.cs

--- LowPolyFishing/Assets/Scripts/PlayerMovement.cs
+++ LowPolyFishing/Assets/Scripts/PlayerMovement.cs
@@ -1,20 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float turnSpeed = 5f;
 
-    
+    Vector2 moveInput;
+    Animator animator;
+
+    float moveXPos;
+    float MoveZPos;
+    string isWalking;
+
     void Start()
     {
-        
+        isWalking = "IsWalking";
+
+        animator = GetComponent<Animator>();
     }
 
     
     void Update()
     {
-        
+        Move();
+    }
+
+
+    void Move()
+    {
+        moveXPos = moveInput.x * turnSpeed * Time.deltaTime;
+        MoveZPos = moveInput.y * moveSpeed * Time.deltaTime;
+
+        if(moveXPos != 0f || MoveZPos != 0f)
+        {
+            animator.SetBool(isWalking, true);
+            transform.Translate(0f, 0f, MoveZPos);
+            transform.Rotate(0f, moveXPos, 0f, Space.Self);
+        }
+        else
+        {
+            animator.SetBool(isWalking, false);
+        }
+    }
+
+
+    void OnMove(InputValue Value)
+    {
+        moveInput = Value.Get<Vector2>();
+        Debug.Log(moveInput);
     }
 }
