a9cca272972f0044f7433218f3eb8049baf53c3c
Grayson Holt <gholtwork@gmail.com>
2022-03-31 18:39:45 -0500

    FishingArea.cs

---
+++ LowPolyFishing/Assets/Scripts/FishingArea.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishingArea : MonoBehaviour
+{
+    PlayerFishing playerFishing;
+
+    void Start()
+    {
+        playerFishing = FindObjectOfType<PlayerFishing>();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Fishing Area Entered");
+            playerFishing.canFish = true;
+        }    
+    }
+
+
+    void OnTriggerEnter(Collision other) 
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Exited Fishing Area");
+            playerFishing.canFish = false;
+        }
+    }
+
+}
