741c84d96e12036d2a0d87405b44933243f91d21
Grayson Holt <gholtwork@gmail.com>
2022-04-02 23:49:21 -0500

    Add Reeling.cs

---
+++ LowPolyFishing/Assets/Scripts/Player/Player Fishing/Reeling.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reeling : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
