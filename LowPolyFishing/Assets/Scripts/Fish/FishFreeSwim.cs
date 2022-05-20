0664278ea00d8fb9b6950562ac5d1b1b973c3bb1
Grayson Holt <gholtwork@gmail.com>
2022-05-19 21:01:07 -0500

    Added FishFreeSwim.cs

---
+++ LowPolyFishing/Assets/Scripts/Fish/FishFreeSwim.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishFreeSwim : MonoBehaviour
+{
+    public float swimSpeed = 5f;
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
